06a46663093cf69687fab3da5ae7ed4da00ae064
Mohammad Amin Ahmadi <amin1382422amin@gmail.com>
2022-12-03 13:37:29 +0330

    Add : MaileRoom Entity and ValueObject

--- 1. Core/Core.Domain/Mailroom/Entities/Mailroom.cs
+++ 1. Core/Core.Domain/Mailroom/Entities/Mailroom.cs
@@ -1,14 +1,12 @@
 ﻿using Framework.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Core.Domain.Mailroom.Entities
 {
-    public class Mailroom:BaseAggregateRoot
-    {
-        /*agg
+	public sealed class Mailroom : BaseAggregateRoot
+	{
+		/*agg
          دبیرخانه که مدیریت کلیه فرایند های ما در این قسمت انجام میشود
         مثل ساخت بخش یا عملیات بایگانی یا مدیریت مدیر های هر بخش*/
-    }
+		public ICollection<Department.Entities.Department> Departments { get; protected set; }
+	}
 }
