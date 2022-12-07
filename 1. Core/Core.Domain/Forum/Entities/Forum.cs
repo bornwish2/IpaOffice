e4509017bca3877b0a7d9997e47c4f0ad6d69d5f
Amin <amin1382422amin@gmail.com>
2022-12-07 13:13:46 +0330

    fix : forum

--- 1. Core/Core.Domain/Forum/Entities/Forum.cs
+++ 1. Core/Core.Domain/Forum/Entities/Forum.cs
@@ -11,13 +11,38 @@
 
         public ICollection<ForumUser> Users { get; set; }
 
+
+
         private Forum() { }
 
         private Forum(ForumUser user) => Users.Add(user);
 
 
+        /// <summary>
+        /// Add user to forum
+        /// </summary>
+        /// <param name="user"></param>
+        public void AddUser(User.Entities.User user) => ForumUser.AddUser(user);
 
 
+        /// <summary>
+        /// Update User in Forum
+        /// </summary>
+        /// <param name="Newuser"></param>
+        /// <param name="oldUser"></param>
+        public void UpdateUser(ForumUser Newuser, ForumUser oldUser)
+        {
+            var user = ForumUser.UpadateUser(Newuser, oldUser);
+            Users.Remove(oldUser);
+            Users.Add(user);
+        }
+
+
+        /// <summary>
+        /// remove user frome Forum
+        /// </summary>
+        /// <param name="User"></param>
+        public void RemoveUser(ForumUser User) => Users.Remove(User);
 
     }
 }
